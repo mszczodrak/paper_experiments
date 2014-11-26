3325e41c22c841195329bea388ea57fabd4b9fe4
Marcin K Szczodrak <msz@cs.columbia.edu>
2014-11-26 13:52:09 -0500

    up

---
+++ indriya/data/cc2420_sync_delay.py
@@ -0,0 +1,24 @@
+#!/usr/bin/python
+# Combine RSSI logs from different TX powers
+# into a single JSON file
+#
+# Author: Marcin K Szczodrak
+# Updated: 5/3/2014
+
+import sys
+import os
+import json
+import cc2420_conf as radio_conf
+sys.path.append("../scripts")
+import tests
+
+if len(sys.argv) != 1:
+	print "usage %s"
+	exit()
+
+channel = 26
+lpl = 0
+pwd = os.path.dirname(os.path.realpath(__file__))
+tests.sync_delay("../cc2420", pwd, "sync_delay_indriya.json")
+
+
