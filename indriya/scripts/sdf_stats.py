8b904bdb117303a83072e75d5b95029bf47ecef2
Marcin K Szczodrak <msz@cs.columbia.edu>
2014-12-12 15:43:42 -0500

    update

--- indriya/scripts/sdf_stats.py
+++ indriya/scripts/sdf_stats.py
@@ -66,7 +66,7 @@ for line in f.readlines():
 
 	if dbg == DBGS_SIGNAL_FINISH_PERIOD:
 		if len(stop_delays[-1]["data"]) > 0  and \
-			timestamp - stop_delays[-1]["data"][-1] > 30:
+			timestamp - stop_delays[-1]["data"][-1] > 21:
 			add_period()
 
 		stop_delays[-1]["data"].append(timestamp)	
