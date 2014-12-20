e75aef642d59c95c88b9cdbbf9792f8c3f3003c5
Marcin K Szczodrak <msz@cs.columbia.edu>
2014-12-20 18:18:42 -0500

    up

--- indriya/data/tests.py
+++ indriya/data/tests.py
@@ -337,7 +337,7 @@ def data_from(root_dir, pwd, title):
 			summary["FROM"] = int(fp.read().rstrip())
 		
 		with open("%s/LOCATION" % (full_path), "r") as fp:
-			summary["LOCATION"] = int(fp.read().rstrip())
+			summary["LOCATION"] = fp.read().rstrip()
 		
 		with open("%s/DELAY" % (full_path), "r") as fp:
 			summary["DELAY"] = int(fp.read().rstrip())
